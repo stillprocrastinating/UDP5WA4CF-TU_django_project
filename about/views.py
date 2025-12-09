31410da7d2f869fc74a4df9d994fb20fdbdbe478
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-12-09 22:23:28 +0000

    django about_me(if-else)

--- about/views.py
+++ about/views.py
@@ -2,4 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def about_me(request):
-    return HttpResponse("This would be the about page")
+    #return HttpResponse("This would be the about page")
+
+    if request.method == "POST":
+        return HttpResponse("You must have POSTed something")
+    else:
+        return HttpResponse(request.method)
