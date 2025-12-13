c4316444b1aca4c1b70ccf951ae8d574e6a652b7
stillprocrastinating <elise.o.hitchcock@outlook.com>
2025-12-13 09:09:54 +0000

    Procfile

--- my_project/settings.py
+++ my_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ujqcw3^sh@x@xb^6a6t4)i$&rll&!d(cqw@=4+0!yq=*dovud('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
